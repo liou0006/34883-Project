ba415c498a79f5a90ec3f7d0134656a22e6a0ba8
DonkeyKong9827 <141904255+DonkeyKong9827@users.noreply.github.com>
2025-01-19 14:05:20 +0100

    added comments to slave and master file and adopted the doxygen layout

--- environmentData/masterCode/masterCode.ino
+++ environmentData/masterCode/masterCode.ino
@@ -1,70 +1,98 @@
-#include <dht.h>
+/**
+*@file masterCode.ino
+*
+*
+*@section description Description
+*Sending data between arduino Uno R3 and ESP8266 NodeMCU while reading values from a temperature sensor (analog) and a humidity sensor (digital)
+*and uploading the data to thingSpeak to show a graph of the humidity and temperature 
+*
+*
+*@section setup Setup
+*Setup:
+*-baud rate = 9600 on both arduino and ESP
+*
+*
+*@section circuit Circuit
+*How to connect the DHT11:
+*-Signal (DHT) to D12 (arduino)
+*-Vcc (DHT) to power (5V - breadboard)
+*-GND (DHT) to GND (breadboard)
+*
+*How to connect TMP36GZ:
+*-Vs (TMP) to GND (breadboard)
+*-Vout (TMP) to A0 (arduino)
+*-Gnd (TMP) to GND (arduino)
+*
+*Setup the breadboard with GND and 5V:
+*-5V (arduino) to + (breadboard)
+*-GND (arduino) to - (breadboard) 
+*
+*How to connect arduino to ESP8266:
+*-D6 (arduino) to Rx (ESP) (IMPORTANT!: when uploading the code to the ESP and arduino - remove this wire from the socket)
+*-D5 (arduino) to Tx (ESP) (IMPORTANT!: when uploading the code to the ESP and arduino - remove this wire from the socket)
+*-GND (ESP) to GND (breadboard)
+*
+*Connect LED to ESP8266:
+*-cathode (LED) to D2 (ESP)
+*-anode (LED) to GND (breadboard) 
+*
+*
+*@section libraries Libraries
+*-dht.h (Download a zip file of this library from this link: https://www.circuitbasics.com/how-to-set-up-the-dht11-humidity-sensor-on-an-arduino/)
+*-SoftwareSerial.h
+*
+*
+*@section author Authors
+*Created by Ask Krat, Christian Houmann
+*
+*
+*@date Today
+*
+**/
 
-dht DHT;
 
-#define DHT11_PIN 12
+#include <dht.h> ///< Include the library for DHT sensors
 
-int data;                    //Initialized variable to store recieved data
-#include <SoftwareSerial.h>  //Included SoftwareSerial Library
-//Started SoftwareSerial at RX and TX pin of ESP8266/NodeMCU
-//defined the pins for softwareserial pins 5 and 6
-SoftwareSerial s(5,6);
+dht DHT; ///< Create a DHT object to interact with the DHT11 sensor
 
+#define DHT11_PIN 12 ///< Define the pin connected to the DHT11 sensor signal line
+
+int data;                    ///< Variable to store received data
+#include <SoftwareSerial.h>  ///< Include the SoftwareSerial library to enable serial communication on other pins
+
+SoftwareSerial s(5,6); ///<// Use Arduino pins 5 and 6 for SoftwareSerial communication with the ESP8266
+
+/** @brief Initialize the serial monitor and the SoftwareSerial communication with ESP8266
+**/
 void setup() {
-  //starts the program
-  Serial.begin(9600);
-  s.begin(9600);
-
-
+  Serial.begin(9600); ///<// Initialize the serial monitor for debugging
+  s.begin(9600); ///< Initialize the SoftwareSerial communication with ESP8266
 
 }
 
+/** @brief Reads the data from the humidity sensor and calculates the temperature value from the analog value from the TMP36 temperature sensor
+*and sends the humidity and temperature to the ESP8266 
+**/
 void loop() {
-  int chk = DHT.read11(DHT11_PIN);
-  // Serial.println("part 1--------- start");
-  // Serial.print("Humidity = ");
-   //Serial.println(DHT.humidity);
-  // Serial.println("part 1--------- end");
-  // delay(1000);
-  // Serial.println("part 2--------- start");
-   int sensorValue = analogRead(A0);
-  // Serial.print("Analog Value: ");
-  // Serial.print(sensorValue);
-  //float voltage = sensorValue * (5.0 / 1023.0);
-  // Serial.print("\tVoltage: ");
-  // Serial.print(voltage);
-  //calculates the tempValue
-   float tempValue = (5.0 / 1023.0) * sensorValue * 100;
-  // Serial.print(" Temperature Value: ");
-  // Serial.print(tempeValue);
-  // Serial.println("\u00B0");
-  // Serial.println("part 2--------- end");
+  int chk = DHT.read11(DHT11_PIN); ///< Read the data from the DHT11 sensor
+   int sensorValue = analogRead(A0); ///< Read the analog value from TMP36 temperature sensor on pin A0
+   float tempValue = (5.0 / 1023.0) * sensorValue * 100; ///< Convert the analog value to a temperature in Celsius
 
-
-  // Format the temperature and humidity as strings
-  //String temperatureMessage = "Temperature: " + String(tempeValue) + "\n";
-  //String humidityMessage = "Humidity: " + String((float) DHT.humidity) + "\n";
-
-  // Send the messages via SoftwareSerial
-  //Serial.println("Sending data...");
-  //mySerial.write((byte*)&tempeValue, sizeof(tempeValue));
-  //mySerial.write((byte*)&DHT.humidity, sizeof(DHT.humidity));
-
-  //mySerial.print(tempeValue);
-  //mySerial.print(DHT.humidity);
-  // if (s.write(123)) {
-    
-  //   Serial.println("hello world");
-  // }
-
-  //s.write(DHT.humidity); // Send the tuple as a string
-  //Serial.println(DHT.humidity);
-  //sends the humidity and tempValue to the ESP8266 module
-  s.print(String(DHT.humidity)+ "," + String(tempValue) + "\n");
-  Serial.println(DHT.humidity);
-  Serial.println(String(tempValue));
-  //Serial.println(DHT.temperature);
-  //Serial.println("hello world");
+  sendingDataToESP(DHT.humidity,tempValue); ///< Send humidity and temperature data to the ESP8266
   
-  delay(2000);
+  delay(2000); ///< Wait for 2 seconds before taking the next reading
 }
+
+/** @brief A function that takes the humidity and temperature as floats and sends it to the ESP8266 module
+* Prints to the serial monitor to double-check that the values are correct
+*@param humidity is Humidity value from the DHT11 sensor
+*@param temperature is the calculated temperature from the TMP36 sensor  
+**/
+void sendingDataToESP(float humidity, float temperature) {
+  s.print(String(humidity)+ "," + String(temperature) + "\n"); ///< Send the data as a formatted string to the ESP8266 over SoftwareSerial
+  Serial.println(humidity); ///< Print the data to the serial monitor
+  Serial.println(String(temperature)); ///< Print the data to the serial monitor
+
+}
+
+
--- environmentData/slaveCode/slaveCode.ino
+++ environmentData/slaveCode/slaveCode.ino
@@ -1,143 +1,151 @@
-//to use the ESP8266
-#include <ESP8266WiFi.h>
-#include <ThingSpeak.h>
-//library for communicating with the arduino uno
-#include <SoftwareSerial.h>
+/**
+*@file slaveCode.ino
+*
+*
+*@section description Description
+*Sending data between arduino Uno R3 and ESP8266 NodeMCU while reading values from a temperature sensor (analog) and a humidity sensor (digital)
+*and uploading the data to thingSpeak to show a graph of the humidity and temperature 
+*
+*
+*@section setup Setup
+*Setup:
+*-baud rate = 9600 on both arduino and ESP
+*
+*
+*@section circuit Circuit
+*How to connect the DHT11:
+*-Signal (DHT) to D12 (arduino)
+*-Vcc (DHT) to power (5V - breadboard)
+*-GND (DHT) to GND (breadboard)
+*
+*How to connect TMP36GZ:
+*-Vs (TMP) to GND (breadboard)
+*-Vout (TMP) to A0 (arduino)
+*-Gnd (TMP) to GND (arduino)
+*
+*Setup the breadboard with GND and 5V:
+*-5V (arduino) to + (breadboard)
+*-GND (arduino) to - (breadboard) 
+*
+*How to connect arduino to ESP8266:
+*-D6 (arduino) to Rx (ESP) (IMPORTANT!: when uploading the code to the ESP and arduino - remove this wire from the socket)
+*-D5 (arduino) to Tx (ESP) (IMPORTANT!: when uploading the code to the ESP and arduino - remove this wire from the socket)
+*-GND (ESP) to GND (breadboard)
+*
+*Connect LED to ESP8266:
+*-cathode (LED) to D2 (ESP)
+*-anode (LED) to GND (breadboard) 
+*
+*
+*@section libraries Libraries
+*-ESP8266WiFi.h (use this link in Settings>Additional...: http://arduino.esp8266.com/stable/package_esp8266com_index.json)
+*-ThingSpeak.h (Download in the Library manager - search for ThingSpeak by MathWorks)
+*-SoftwareSerial.h
+*
+*
+*@section author Authors
+*Created by Ask Krat, Christian Houmann
+*
+*
+*@date Today
+*
+**/
+
+
+
+// Include libraries for ESP8266, ThingSpeak, and SoftwareSerial
+#include <ESP8266WiFi.h> ///<Library for connecting the ESP8266 to WiFi
+#include <ThingSpeak.h> ///<Library for communicating with ThingSpeak
+#include <SoftwareSerial.h> ///<library for communicating with the arduino uno
+
 // Wi-Fi configuration
-const char* ssid = "Ask Krat";
-const char* pass = "feature-hollow-truly";
-WiFiClient client;
+const char* ssid = "Ask Krat"; ///< Wi-Fi network name
+const char* pass = "feature-hollow-truly"; ///< Wi-Fi password
+WiFiClient client;  ///< Create a Wi-Fi client object
+
 // ThingSpeak configuration
-unsigned long channelID = 2808283;
-// Replace with your ThingSpeak channel ID
-//the data from ThingSpeak
-const char* APIWriteKey = "G4QFBJM48LQQLI4T";  // Replace with your Write API Key
-const char* APIReadKey = "PUSZ92SJXXMO8BDG";
-const int postDelay = 20 * 1000;  // 20 seconds delay
-//int ledPin = D2;  // Pin connected to the LED (D4 for built-in LED on ESP8266)
-int data;  //Initialized variable to store recieved data
+unsigned long channelID = 2808283; ///< ThingSpeak channel ID
+const char* APIWriteKey = "G4QFBJM48LQQLI4T";  ///< Write API key for ThingSpeak
+const char* APIReadKey = "PUSZ92SJXXMO8BDG"; ///< Read API key for ThingSpeak
+const int postDelay = 20 * 1000;  ///< Delay between data posts (20 seconds)
 
 //the pins on the ESP8266
-SoftwareSerial mySerial(1, 3);  //RX, TX
+SoftwareSerial mySerial(1, 3);  ///< SoftwareSerial for communication with ESP8266 (pins RX=1, TX=3)
 
-
-char buffer[50];      // Buffer to store incoming data
-int bufferIndex = 0;  // Index to track the buffer
-
+/** @brief Tries to connect to the WiFi and if it is a success, it begins the communication with ThingSpeak
+**/
 void setup() {
-  Serial.begin(9600);
-  // // Initialize serial communication for debugging
-  // //pinMode(ledPin, OUTPUT);    // Set LED pin as output
-  // //digitalWrite(ledPin, LOW);  // Turn off the LED initially
-  // Serial.println("Connecting to WiFi...");
-  // WiFi.begin(ssid, pass);  // Connect to WiFi
-  //starting wifi
-  WiFi.begin(ssid, pass);
+  Serial.begin(9600); ///< Initialize Serial Monitor for debugging
+  
+  WiFi.begin(ssid, pass); ///< Start Wi-Fi connection with the given SSID and password
 
-  // //   // Wait for WiFi connection
-  //    while (WiFi.status() != WL_CONNECTED) {
-  //      delay(500);
-  //      Serial.print(".");
-  //    }
-  //    Serial.println("\nConnected to WiFi");
-  //    Serial.print("IP Address: ");
-  //    Serial.println(WiFi.localIP());  // Print the assigned IP address
-  // }
-  //checks if there is WiFi
+  // Wait until Wi-Fi is connected
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
-  Serial.println("Connected to WiFi");
-  ThingSpeak.begin(client);
+  Serial.println("Connected to WiFi"); ///< Confirm Wi-Fi connection
+  ThingSpeak.begin(client); ///< Initialize ThingSpeak client
 }
 
+/** @brief Converts the received string from the Serial to two floats and sends the floats to ThingSpeak into two seperate graphs
+*-One graph for humidity (the 1st value - Channel 1)
+*-One graph for temperature (the 2nd value - Channel 2)
+**/
 void loop() {
-  // // Get RSSI value
-  //float rssi = WiFi.RSSI();
-
-  // // // Toggle LED state
-  // // // static bool ledState = false;                 // Track LED state (on/off)
-  // // // ledState = !ledState;                         // Toggle state
-  // // // digitalWrite(ledPin, ledState ? HIGH : LOW);  // Update LED
-
-  // // // Print debug information
-  //Serial.print("WiFi RSSI: ");
-  //Serial.println(rssi);
-  // //Serial.print("LED State: ");
-  // //Serial.println(ledState ? "ON" : "OFF");
-
-  // // // Send data to ThingSpeak
-  //ThingSpeak.begin(client);
-  //ThingSpeak.setField(1, rssi);              // Send RSSI to Field 1
-  //  //ThingSpeak.setField(2, ledState ? 1 : 0);  // Send LED state to Field 2 (1 for ON, 0 for OFF)
-
-  // if (ThingSpeak.writeFields(channelID, APIWriteKey)) {
-  //   Serial.println("Data sent to ThingSpeak successfully.");
-  // } else {
-  //   Serial.println("Failed to send data to ThingSpeak.");
-  // }
-
-  // // // Read the value from Field 1 (RSSI) using the Read API key
-  // float field1Value = ThingSpeak.readFloatField(channelID, 1, APIReadKey);
-  // if (!isnan(field1Value)) {
-  //   Serial.print("Field 1 (RSSI) value from ThingSpeak: ");
-  //   Serial.println(field1Value);
-  // } else {
-  //   Serial.println("Failed to read Field 1 value from ThingSpeak.");
-  // }
-
-  // client.stop();     // Close the connection
-  // delay(postDelay);  // Wait before the next update
-
-  //data = Serial.read();  //Read the serial data and store it
-  //Serial.println(data);
-  //Serial.write(Serial.read());
-
   //handles the recieved data string and converts it into two floats
-  String receivedData = Serial.readStringUntil('\n');  // Read until newline
+  String receivedData = Serial.readStringUntil('\n');  ///< Read data from Serial until newline character
+  
   // Parse the received tuple
-  int commaIndex = receivedData.indexOf(',');
-  if (commaIndex > 0) {
-    String value1 = receivedData.substring(0, commaIndex);
-    String value2 = receivedData.substring(commaIndex + 1);
-    // Send values to ThingSpeak
-
+  int commaIndex = receivedData.indexOf(','); ///< Find the comma in the received string
+  if (commaIndex > 0) { ///< Ensure a valid comma is found
+    String value1 = receivedData.substring(0, commaIndex); ///< Extract first value (before the comma)
+    String value2 = receivedData.substring(commaIndex + 1); ///< Extract second value (after the comma)
+    
+    // Validate and send values to ThingSpeak if they are within valid ranges
     if ((value1.toFloat() > -50.0 && (value1.toFloat() < 50.0)) && (value2.toFloat() > 0.0 && (value2.toFloat() < 100.0))) {
-
-      //writes the integer to ThingSpeak field 1
-    if (ThingSpeak.setField(1, value1.toFloat())) {
-      Serial.println("Field 1 updated");
-      Serial.println(value1);
-    } else {
-      Serial.println("Failed to update Field 1");
-    }
-
-    //writes the integer to ThingSpeak field 2
-    if (ThingSpeak.setField(2, value2.toFloat())) {
-      Serial.println(value2);
-      Serial.println("Field 2 updated");
-    } else {
-      Serial.println("Failed to update Field 1");
-    }
-    // if (ThingSpeak.writeField(myChannelNumber, 2, value2.toInt(), myWriteAPIKey) == 200) {
-    //   Serial.println("Field 2 updated");
-    // } else {
-    //   Serial.println("Failed to update Field 2");
-    // }
+      sendDataToThingSpeak(value1,value2); ///< Send validated data to ThingSpeak
   }
   }
   
-  //checks if the upload to ThingSpeak was succesfull
-  int responseCode = ThingSpeak.writeFields(channelID, APIWriteKey);
+  //checks if the upload to ThingSpeak was succesful
+  int responseCode = ThingSpeak.writeFields(channelID, APIWriteKey); ///< Write fields to ThingSpeak
 
-  if (responseCode == 200) {
+  if (responseCode == 200) { ///< HTTP response code 200 indicates success
     Serial.println("Data sent success");
-  } else {
+  } else { ///< Other codes indicate failure
     Serial.println("Failed to send");
   }
 
 
-  delay(1000);  // Wait for 1 second before next loop
+  delay(1000);  // Wait for 1 second before the next loop iteration
 }
+
+/** @brief Sends the humidity and temperature to ThingSpeak
+*-the first value (humidity) is send to field 1
+*-the 2nd value (temperature) is send to field 2
+*@param value1 is the humidity as a percentage
+*@param value2 is the temperature (in celcius)
+**/
+void sendDataToThingSpeak(String value1, String value2) {
+   // Converts the first value from a string to a float and writes it to ThingSpeak field 1 
+    if (ThingSpeak.setField(1, value1.toFloat())) {
+      Serial.println("Field 1 updated");
+      Serial.println(value1); ///< Print the updated value for debugging
+    } else {
+      Serial.println("Failed to update Field 1"); ///< Log failure
+    }
+
+    // Converts the 2nd value from a string to a float and writes it to ThingSpeak field 1 
+    if (ThingSpeak.setField(2, value2.toFloat())) {
+      Serial.println(value2); ///< Print the updated value for debugging
+      Serial.println("Field 2 updated"); 
+    } else {
+      Serial.println("Failed to update Field 1"); ///< Log failure
+    }
+}
+
+
+
+
 
