8e9b495d5f361e1c2f6736146fe7fe55437a0436
DonkeyKong9827 <141904255+DonkeyKong9827@users.noreply.github.com>
2025-01-20 13:18:50 +0100

    added doxygen documentation

--- sketch_ESP_Room/sketch_ESP_Room.ino
+++ sketch_ESP_Room/sketch_ESP_Room.ino
@@ -1,4 +1,58 @@
 /**
+*@file masterCode.ino
+*
+*
+*@section description Description
+*Sending data between arduino Uno R3 and ESP8266 NodeMCU while reading values from a temperature sensor (analog) and a humidity sensor (digital)
+*and uploading the data to thingSpeak to show a graph of the humidity and temperature 
+*
+*
+*@section setup Setup
+*Setup:
+*-baud rate = 9600 on both arduino and ESP
+*
+*
+*@section circuit Circuit
+*How to connect the DHT11:
+*-Signal (DHT) to D12 (arduino)
+*-Vcc (DHT) to power (5V - breadboard)
+*-GND (DHT) to GND (breadboard)
+*
+*How to connect TMP36GZ:
+*-Vs (TMP) to GND (breadboard)
+*-Vout (TMP) to A0 (arduino)
+*-Gnd (TMP) to GND (arduino)
+*
+*Setup the breadboard with GND and 5V:
+*-5V (arduino) to + (breadboard)
+*-GND (arduino) to - (breadboard) 
+*
+*How to connect arduino to ESP8266:
+*-D6 (arduino) to Rx (ESP) (IMPORTANT!: when uploading the code to the ESP and arduino - remove this wire from the socket)
+*-D5 (arduino) to Tx (ESP) (IMPORTANT!: when uploading the code to the ESP and arduino - remove this wire from the socket)
+*-GND (ESP) to GND (breadboard)
+*
+*Connect LED to ESP8266:
+*-cathode (LED) to D2 (ESP)
+*-anode (LED) to GND (breadboard) 
+*
+*
+*@section libraries Libraries
+*-dht.h (Download a zip file of this library from this link: https://www.circuitbasics.com/how-to-set-up-the-dht11-humidity-sensor-on-an-arduino/)
+*-SoftwareSerial.h
+*
+*
+*@section author Authors
+*Created by Ask Krat, Christian Houmann
+*
+*
+*@date Today
+*
+**/
+
+
+
+/**
    @file sketch_ESP_Room.ino
 
    @mainpage This is a mainpage
