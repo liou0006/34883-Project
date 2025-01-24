53d231f7e5ca3d4bd64f4da51c045d5a2bcb936c
The Cool Guy <75526391+LUKTALL@users.noreply.github.com>
2025-01-24 12:43:52 +0100

    Refactored doxygen description

--- sketch_Arduino_Room/sketch_Arduino_Room.ino
+++ sketch_Arduino_Room/sketch_Arduino_Room.ino
@@ -5,8 +5,6 @@
 *@section description Description
 *Logs data from the sensors and displays them on an LCD. Data is also sent to an ESP8266 through UART for transmitting to ThingSpeak.
 *
-*
-*
 *@section circuit Circuit
 *<b>DHT11 wiring</b>\n 
 *-Pin 1 (Signal) to D4.
